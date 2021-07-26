7a852f7a33b561210014f2b357e816ba51e81d10
dariusghiura <ghiuradarius@gmail.com>
2021-07-26 12:02:01 +0300

    Created a form that adds a new post

--- js/script.js
+++ js/script.js
@@ -2,3 +2,28 @@ function hidePost(el) {
     console.log("clcik");
     el.parentNode.parentNode.style.display = 'none';
 }
+
+function formSubmit() {
+    console.log("submit");
+    let image = document.forms["post-form"]["image"].value;
+    let description = document.forms["post-form"]["description"].value;
+    if (image == "" || description == "") {
+        alert("Both fields required")
+    } else {
+        createPost(image, description);
+    }
+}
+
+function createPost(image, description) {
+    console.log("./assets/images/" + image)
+    let picture = document.getElementsByClassName("post-picture")[0];
+    let desc = document.getElementsByClassName("description")[0];
+    let img = document.createElement("img");
+    img.src = "./assets/images/" + image;
+    let p = document.createElement("p");
+    p.innerHTML = description;
+    picture.appendChild(img);
+    desc.appendChild(p);
+    let post = document.getElementsByClassName("post")[0];
+    post.style.display = 'inline';
+}
